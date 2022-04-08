e13e1a2ee4020d5edf2c455427531f85faae09f8
Bruna <brunac.cardososilva@gmail.com>
2022-04-08 18:43:49 -0300

    Req 17 - Criar - Implementado o botão para home

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Ranking extends Component {
   render() {
     return (
-      <div data-testid="ranking-title">Ranking</div>
+      <div>
+        <div data-testid="ranking-title">Ranking</div>
+        <Link to="/">
+          <button
+            data-testid="btn-go-home"
+            type="button"
+          >
+            início
+          </button>
+        </Link>
+      </div>
     );
   }
 }
